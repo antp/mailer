68ed59c395746e684fe5ca470a238107b739ebd7
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-12-26 14:08:09 +0530

    Fix failing tests because of empty cc and bcc

--- lib/mailer/smtp_client.ex
+++ lib/mailer/smtp_client.ex
@@ -1,18 +1,20 @@
 defmodule Mailer.Smtp.Client do
   def send(from, to, content, server) do
+    # remove empty cc and bcc headers which can cause errors in encoding
+    headers = Enum.reject(elem(content, 2), fn {k, v} -> k in ["Cc", "Bcc"] and v == [] end)
+    content = put_elem(content, 2, headers)
     encoded = :mimemail.encode(content)
 
+    server_cfg = Application.get_env(:mailer, :smtp_client)
 
-      server_cfg = Application.get_env(:mailer, :smtp_client)
-
-      handler = List.foldr(server_cfg, nil, fn({k,v}, acc) ->
-        case k do
-          :transport ->
-            v
-          _ ->
-            acc
-        end
-      end)
+    handler = List.foldr(server_cfg, nil, fn({k,v}, acc) ->
+      case k do
+        :transport ->
+          v
+        _ ->
+          acc
+      end
+    end)
 
     do_send(handler, from, to, encoded, server)
   end
